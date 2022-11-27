aa743ccea6876bd9236c5c452d87189a2004ef50
Karen Gonzalez <kagoncaoca@gmail.com>
2022-11-27 17:55:29 -0300

    app with react

---
+++ src/components/ContentWrapper.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ContentRowTop from "./ContentRowTop";
+import Footer from "./Footer";
+import TopBar from "./TopBar";
+
+const ContentWrapper = () => {
+  return (
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <TopBar />
+        <ContentRowTop />
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default ContentWrapper;
